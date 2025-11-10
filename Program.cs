734ed5b562f87e765177ed4ae5d7dad0a167e78f
Gabriel De Souza Cavalheiro <476849624@ist.senai>
2025-11-10 08:05:46 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿Console.Clear(); // => Limpa o console quando o código é executado
+/* <- -> comenta tudo
+string meuTexto = "Hello World!";
+
+Console.WriteLine(meuTexto);
+
+// Int - -156 \ 3233
+// Float - -15.6 \ 32.33
+// Bool - true \ false
+// String - "Texto em aspas duplas"
+// Char - 'a' -> caracteres em aspas simples
+
+// Variável - regras p/ nome: 
+// Pode:
+// A-Z 
+// 0-9 -> desde de que não seja o primeiro caractere
+// _
+
+int segundaGuerraMundial = 1942;
+string cor_favorita = "Laranja";
+float velocidadeF1 = 294.48f; // para float -> número decimal float deve ter 'f' no final do número
+bool segundaGuerraMundialAconteceu = true;
+
+// Não pode:
+// @ $ .
+// Palavras da linguagem C#
+// Variáveis com o mesmo nome no mesmo lugar
+
+Console.WriteLine(segundaGuerraMundial);
+Console.WriteLine(cor_favorita);
+Console.WriteLine(velocidadeF1);
+Console.WriteLine(segundaGuerraMundialAconteceu);
+
+velocidadeF1 = 348.29f; // = -> atribuição
+cor_favorita = "Roxo";
+
+Console.WriteLine(velocidadeF1);
+Console.WriteLine(cor_favorita);
+*/
+
+
+/*
+var cor_favorita = "Vermelho"; // C# adivinha a variável -> string
+var modeloDoProduto = "2323"; // -> int 
+*/
+
+
+/*
+const float PI = 3.14159265359f; // Por ser uma constante, não pode ser mudada, ao contrário das variáveis
+*/
+
+
+/*
+Console.Write("Escreva seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Seu nome é: {nome}");
+*/
+
+
+/*
+int numeroQualquer = 20 + 20 - 80 * 5;
+
+int numeroDivisao = 6 / 2;
+int numeroDivisaoDecimal = 5 / 2; // int-> 2.5 => 2
+float numeroDiv = 5f / 2;
+
+int matematicaReagras = 2 + 2 * 10; // 22 => C# segue as regras da matemática
+int matematicaRegras2 = (2 + 2) * 10; // 40
+
+Console.WriteLine($"{numeroQualquer}\n{numeroDivisao}\n{numeroDivisaoDecimal}\n{numeroDiv}\n{matematica}");
+*/
+
+
+/*
+// Condicões:
+// > - maior
+// >= - maior ou igual
+// < - menor
+// <= - menor ou igual
+// == - comparação
+// != - diferente
+// true / false
+// && -> e / and
+// || -> ou / or
+
+Console.Write("Digite sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade < 18) // if => "se"
+{
+    Console.Write("Menor de idade.");
+}
+else if (idade >= 18 && idade < 60) // else if => "se senão"
+{
+    Console.WriteLine("Maior de idade.");
+}
+else // else => "senão"
+{
+    Console.Write("Idoso.");
+}
+*/
+
+
+/*
+// Cast:
+// .Parse => converter
+*/
+
+
+
+// Funções:
+// 
+
+// 3:13:45
